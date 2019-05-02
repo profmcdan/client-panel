2b1e8a0e2c35471dcd0982e75e295c9cdf6b58ec
Daniel Ale <denielale9291@gmail.com>
2019-05-02 22:55:31 +0100

    Initial Commit

---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AppNavbar = () => (
+  <div className="container">
+    <h1>Header</h1>
+  </div>
+);
+
+export default AppNavbar;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Footer = () => <footer>2019</footer>;
+
+export default Footer;
