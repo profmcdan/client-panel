75f2dd8fcaab3f859a521172237fc055b7ac945d
Daniel Ale <denielale9291@gmail.com>
2019-05-03 08:33:26 +0100

    Configure redux firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./App.css";
 import Footer from "./components/layout/Footer";
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/home/Dashboard";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <AppNavbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-        </Switch>
-      </div>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <AppNavbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
 
